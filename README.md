d52f2356ca4943c5437765b69ef91dbbd7918103
Adam <nuradam3033@gmail.com>
2024-01-01 16:24:43 -0600

    Employee Management Application

---
+++ README.md
@@ -0,0 +1,60 @@
+# Employee Management Application
+
+This command-line application is designed to help you manage departments, roles, and employees in a company. It provides a user-friendly interface with various options to view and update information within the database.
+
+## Features
+
+- **View all departments:** Display a formatted table showing department names and IDs.
+- **View all roles:** Present job titles, role IDs, associated departments, and salaries.
+- **View all employees:** Show employee data, including IDs, first names, last names, job titles, departments, salaries, and manager information.
+- **Add a department:** Easily add a new department by entering its name.
+- **Add a role:** Add a new role by providing the name, salary, and associated department.
+- **Add an employee:** Enter employee details such as first name, last name, role, and manager to add a new employee to the database.
+- **Update an employee role:** Select an employee and their new role to update their information in the database.
+
+## Usage
+
+1. **Starting the Application:**
+   - Run the application in your command-line interface.
+
+2. **Main Menu:**
+   - Upon starting, you will be presented with a menu containing the following options:
+     - View all departments
+     - View all roles
+     - View all employees
+     - Add a department
+     - Add a role
+     - Add an employee
+     - Update an employee role
+
+3. **Options Description:**
+   - Select an option by entering the corresponding number.
+
+4. **Viewing Information:**
+   - Choose options 1-3 to view information in formatted tables.
+
+5. **Adding Information:**
+   - Choose options 4-6 to add new departments, roles, or employees. Follow the prompts to enter relevant information.
+
+6. **Updating Employee Role:**
+   - Choose option 7 to update an employee's role. Follow the prompts to select an employee and provide their new role.
+
+7. **Exiting the Application:**
+   - Choose an option to exit the application.
+
+8. **------------------------------------------------------------**
+   ![Alt text](image.png)![Alt text](image-1.png)![Alt text](image-2.png)
+
+## Installation
+
+1. Clone the repository to your local machine.
+   ```bash
+   git clone https://github.com/Nurshat01/Employee-Tracker-DB.git
+
+     - To install 'npm i inquirer@8.2.4' 
+     - mysql2 'npm i mysql2' 
+     - node 'npm init -y' 
+     - console.table  'npm i console.table' 
+
+## video
+1 video: https://1drv.ms/v/s!At889SuyUenyiSQHKR6u9T7DZHfN?e=A7y1dg
---
+++ server.js
@@ -0,0 +1,268 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const consoleTable = require("console.table");
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "xxxx",
+    password: "xxxx",
+    database: "employee_db",
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("**************** You are succesfully connected. ********************");
+    tracker();
+});
+
+function tracker() {
+    inquirer
+        .prompt ({
+            name: "action",
+            type: "list",
+            message: "Please select options below?",
+            choices: [
+                "View All Employees",
+                "Add Employee",
+                "Update Employee Role",
+                "View All Roles",
+                "Add Role",
+                "View All Departments",
+                "Add Department",
+                "Quit",
+            ],
+        })
+
+        .then((answer) => {
+            if (answer.action === "View All Employees") {
+                viewAllEmployees();
+            } else if (answer.action === "Add Employee") {
+                addEmployee();
+            } else if (answer.action === "Update Employee Role") {
+                updateEmployeeRole();
+            } else if (answer.action === "View All Roles") {
+                viewAllRoles();
+            } else if (answer.action === "Add Role") {
+                addRole();
+            } else if (answer.action === "View All Departments") {
+                viewAllDepartments();
+            } else if (answer.action === "Add Department") {
+                addDepartment();
+            } else if (answer.action === "Quit") {
+                connection.end();
+            } else {
+                console.log("Invalid, try again.");
+                tracker();
+            }
+        });
+}
+
+function viewAllEmployees() {
+    connection.query(
+        'SELECT employees.id, employees.first_name, employees.last_name, roles.title, department.name AS department, roles.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id LEFT JOIN employees AS manager ON employees.manager_id = manager.id',
+        (err, res) => {
+            if (err) throw err;
+            console.table(res);
+            tracker();
+        }
+    );
+}
+
+function addEmployee() {
+    connection.query("SELECT * FROM roles", (err, roles) => {
+        if (err) throw err;
+        connection.query("SELECT * FROM employees", (err, employees) => {
+            if (err) throw err;
+            inquirer
+                .prompt([
+                    {
+                        name: "firstName",
+                        type: "input",
+                        message: "Enter first name of employee:",
+                    },
+                    {
+                        name: "lastName",
+                        type: "input",
+                        message: "Enter last name of employee:",
+                    },
+                    {
+                        name: "role",
+                        type: "list",
+                        message: "Select role of employee:",
+                        choices: roles.map((role) => role.title),
+                    },
+                    {
+                        name: "manager",
+                        type: "list",
+                        message: "Select employee's manager:",
+                        choices: [
+                            "None",
+                            ...employees.map(
+                                (employees) => `${employees.first_name} ${employees.last_name}`
+                            ),
+                        ],
+                    },
+                ])
+                .then((answer) => {
+                    const selectedRole = roles.find(
+                        (roles) => roles.title === answer.role
+                    );
+                    let managerId = null;
+                    if (answer.manager !== "None") {
+                        const selectedManager = employees.find(
+                            (employees) =>
+                                `${employees.first_name} ${employees.last_name}` ===
+                                answer.manager
+                        );
+                        managerId = selectedManager.id;
+                    }
+                    connection.query(
+                        "INSERT INTO employees SET ?",
+                        {
+                            first_name: answer.firstName,
+                            last_name: answer.lastName,
+                            role_id: selectedRole.id,
+                            manager_id: managerId,
+                        },
+                        (err) => {
+                            if (err) throw err;
+                            console.log ("New employee added!");
+                            tracker();
+                        }
+                    );
+                });
+        });
+    });
+}
+
+function updateEmployeeRole() {
+    connection.query("SELECT * FROM employees", (err, employees) => {
+        if (err) throw err;
+        connection.query("SELECT * FROM roles", (err, roles) => {
+            inquirer
+                .prompt([
+                    {
+                        name: "employee",
+                        type: "list",
+                        message: "Select employee to update:",
+                        choices: employees.map(
+                            (employees) => `${employees.first_name} ${employees.last_name}`
+                        ),
+                    },
+                    {
+                        name: "role",
+                        type: "list",
+                        message: "Select role of employee:",
+                        choices: roles.map((role) => role.title),
+                    },
+                ])
+                .then((answer) => {
+                    const selectedEmployees = employees.find(
+                        (employees) =>
+                            `${employees.first_name} ${employees.last_name}` ===
+                            answer.employees
+                    );
+                    const selectedRole = roles.find(
+                        (role) => role.title === answer.role
+                    );
+
+                    connection.query(
+                        "UPDATE employees SET role_id = ? WHERE id = ?",
+                        [selectedRole.id, selectedEmployees.id],
+                        (err) => {
+                            if (err) throw err;
+                            console.log("Employee role is updated!");
+                            tracker();
+                        }
+                    );
+                });
+        });
+    })
+}
+
+function viewAllRoles() {
+    connection.query(
+        "SELECT roles.id, roles.title, roles.salary, department.name AS department FROM roles INNER JOIN department ON roles.department_id = department.id",
+        (err, res) => {
+            if (err) throw err;
+            console.table(res);
+            tracker();
+        }
+    );
+}
+
+function addRole() {
+    connection.query("SELECT * FROM department", (err, departments) => {
+        if (err) throw err;
+
+        inquirer
+            .prompt([
+                {
+                    name: "title",
+                    type: "input",
+                    message: "Enter name of role:",
+                },
+                {
+                    name: "salary",
+                    type: "input",
+                    message: "Enter salary of role:",
+                },
+                {
+                    name: "department",
+                    type: "list",
+                    message: "Select department of role:",
+                    choices: departments.map((department) => department.name),
+                },
+            ])
+            .then((answer) => {
+                const selectedDepartment = departments.find(
+                    (department) => department.name === answer.department
+                );
+                connection.query(
+                    "INSERT INTO roles SET ?",
+                    {
+                        title: answer.title,
+                        salary: answer.salary,
+                        department_id: selectedDepartment.id,
+                    },
+                    (err) => {
+                        if (err) throw err;
+                        console.log("Role has been added!");
+                        tracker();
+                    }
+                );
+            });
+    });
+}
+
+function viewAllDepartments() {
+    connection.query("SELECT * FROM department", (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        tracker();
+    });
+}
+
+function addDepartment() {
+    inquirer
+        .prompt({
+            name: "department",
+            type: "input",
+            message: "Enter department name:",
+        })
+        .then((answer) => {
+            connection.query(
+                "INSERT INTO department SET ?",
+                {
+                    name: answer.department,
+                },
+                (err) => {
+                    if (err) throw err;
+                    console.log("Department has been added!");
+                    tracker();
+                }
+            );
+        });
+}
